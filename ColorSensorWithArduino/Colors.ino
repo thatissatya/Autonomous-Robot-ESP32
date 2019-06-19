772bfd3e2d9c45da318f471c4e6a36e8e5c179a7
Satya Prakash <satyaprakashkumar75@gmail.com>
2019-06-19 19:42:10 +0530

    Add files via upload

---
+++ ColorSensorWithArduino/Colors.ino
@@ -0,0 +1,101 @@
+// TCS230 or TCS3200 pins wiring to Arduino
+#define S0 4
+#define S1 5
+#define S2 6
+#define S3 7
+#define sensorOut 8
+
+// Stores frequency read by the photodiodes
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+
+// Stores the red. green and blue colors
+int redColor = 0;
+int greenColor = 0;
+int blueColor = 0;
+
+void setup() {
+  // Setting the outputs
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  // Setting the sensorOut as an input
+  pinMode(sensorOut, INPUT);
+  // Setting frequency scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  // Begins serial communication
+  Serial.begin(9600);
+}
+
+
+
+
+void loop() {
+  // Setting RED (R) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  
+  // Reading the output frequency
+  redFrequency = pulseIn(sensorOut, LOW);
+  // Remaping the value of the RED (R) frequency from 0 to 255
+  // You must replace with your own values. Here's an example: 
+  // redColor = map(redFrequency, 70, 120, 255,0);
+   redColor = map(redFrequency, 294, 450, 255,0);
+  
+  // Printing the RED (R) value
+ 
+  
+ // Serial.print("R = ");
+ // Serial.print(redFrequency);
+ // delay(100);
+  
+  // Setting GREEN (G) filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  greenFrequency = pulseIn(sensorOut, LOW);
+  // Remaping the value of the GREEN (G) frequency from 0 to 255
+  // You must replace with your own values. Here's an example: 
+  // greenColor = map(greenFrequency, 100, 199, 255, 0);
+  greenColor = map(greenFrequency, 275, 420, 255, 0);
+  
+  // Printing the GREEN (G) value  
+  //Serial.print(" G = ");
+ // Serial.print(greenFrequency);
+  delay(100);
+ 
+  // Setting BLUE (B) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  blueFrequency = pulseIn(sensorOut, LOW);
+  // Remaping the value of the BLUE (B) frequency from 0 to 255
+  // You must replace with your own values. Here's an example: 
+  // blueColor = map(blueFrequency, 38, 84, 255, 0);
+  blueColor = map(blueFrequency, 202, 300, 255, 0);
+  
+  // Printing the BLUE (B) value 
+  //Serial.print(" B = ");
+ // Serial.println(blueFrequency);
+  //delay(100);
+
+//  // Checks the current detected color and prints
+//  // a message in the serial monitor
+  if(redColor > greenColor && redColor > blueColor){
+      Serial.println(" RED detected!");
+  }
+  if(greenColor > redColor && greenColor > blueColor){
+    Serial.println("GREEN detected!");
+  }
+  if(blueColor > redColor && blueColor > greenColor){
+    Serial.println("BLUE detected!");
+  }
+  delay(2000);
+
+  Serial.write(13);
+}
